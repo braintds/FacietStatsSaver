0095ec816ed53a3cbd8c389b40b9494f113e7afa
braintds <freestyle.ff50@gmail.com>
2026-05-01 18:51:21 +0700

    added: statistic calculation service

---
+++ FacietStatsSaver/Services/IStatisticsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacietStatsSaver.Services
+{
+    public interface IStatisticsService
+    {
+        double CalculateKD(IEnumerable<Domain.Stats> matches);
+        double CalculateAVG(IEnumerable<Domain.Stats> matches);
+
+        double CalculateHS(IEnumerable<Domain.Stats> matches);
+
+        double CalcutateWins(IEnumerable<Domain.Stats> matches);
+
+        double CalcutateLoses(IEnumerable<Domain.Stats> matches);
+    }
+}
---
+++ FacietStatsSaver/Services/StatisticsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FacietStatsSaver.Domain;
+
+namespace FacietStatsSaver.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        public double CalculateKD(IEnumerable<Domain.Stats> matches) => matches.Sum(x => x.KDRatio) / matches.Count();
+
+        double IStatisticsService.CalculateHS(IEnumerable<Stats> matches) => matches.Sum(x => x.HeadshotsPercentage) / matches.Count();
+
+        double IStatisticsService.CalculateAVG(IEnumerable<Stats> matches) => matches.Sum(x => x.Kills) / matches.Count();
+
+        double IStatisticsService.CalcutateWins(IEnumerable<Stats> matches) => matches.Count(x => x.Result == "Win");
+
+        double IStatisticsService.CalcutateLoses(IEnumerable<Stats> matches) => matches.Count(x=>x.Result == "Lose");
+    }
+}
